dcc43f56644e7ff44be43524c1cd7d1d4427ff2a
ovi100 <ovi0167@hotmail.com>
2020-01-03 12:37:14 +0600

    Site done without responsive

--- js/main.js
+++ js/main.js
@@ -19,8 +19,12 @@ $(document).ready(function() {
 
     $('.sidenav-toggle-btn').click(function() {
         /* Act on the event */
-        $('#sideNav').toggleClass('show-sidemenu');
-        $('.site-wrapper').toggleClass('toggle-content');
+        $('.sideNav').toggleClass('show-sidemenu');
+        $('.left-content').toggleClass('col-sm-2');
+        $('.right-content').toggleClass('col-sm-10');
+        $('.back-link').toggleClass('container');
+        $('.collection').toggleClass('container');
+        $('.footer-content').toggleClass('container');
     });
 
     $('.sideNav-overlay').click(function() {
@@ -68,7 +72,7 @@ $(document).ready(function() {
 
     $('.hp-slider').owlCarousel({
         loop:true,
-        margin:10,
+        margin:12,
         dots:false,
         nav:true,
         navText: [
@@ -81,9 +85,59 @@ $(document).ready(function() {
             },
             767:{
               items:2
+            },
+            1920:{
+              items:3
             }
         }
     });
+
+    /*
+        * ----------------------------------------------
+        *--------------Home Product Slider--------------
+        * ----------------------------------------------
+    */
+
+    $('.collection-slider').owlCarousel({
+        autoplay: true,
+        loop:true,
+        margin:10,
+        dots:false,
+        //nav:true,
+        /*navText: [
+            '<i class="icofont-thin-left"></i>',
+            '<i class="icofont-thin-right"></i>'
+        ],*/
+        responsive:{
+            0:{
+              items:2
+            },
+            770:{
+              items:3
+            },
+            1200:{
+              items:4
+            }
+        }
+    });
+
+    /*
+        * ----------------------------------------------
+        *--------------Product Details Slider--------------
+        * ----------------------------------------------
+    */
+
+    $('.pd-slider').owlCarousel({
+        loop:true,
+        margin:10,
+        dots:false,
+        items: 1,
+        URLhashListener: true,
+        autoplayHoverPause: true,
+        startPosition: 'URLHash'
+    });
+
+    
 
     /*
         * ----------------------------------------------
@@ -91,12 +145,15 @@ $(document).ready(function() {
         * ----------------------------------------------
     */
 
-    /*$('.rp-slider').owlCarousel({
+    $('.rp-slider').owlCarousel({
         loop:true,
         margin:10,
-        dots: true,
+        dots: false,
         nav:true,
-        navText: ['<i class="icofont-thin-left"></i>','<i class="icofont-thin-right"></i>'],
+        navText: [
+            '<i class="icofont-thin-left"></i>',
+            '<i class="icofont-thin-right"></i>'
+        ],
         responsive:{
             0:{
               items:2
@@ -111,7 +168,7 @@ $(document).ready(function() {
               items:4
             }
         }
-    });*/
+    });
 
     /*
     * -----------------------------------------------------------------
@@ -120,11 +177,17 @@ $(document).ready(function() {
     */
 
 
-    $(".pdd-img").elevateZoom({
-        gallery:'controls',
-        galleryActiveClass: 'active',
+    $(".pimg-zoom").elevateZoom({
+        cursor: "pointer",
+        borderSize: 1,
+        //gallery:'thumbnails',
+        //galleryActiveClass: 'active',
+        responsive: true,
+        zoomWindowWidth: 845,
+        zoomWindowFadeIn: 500,
+        zoomWindowFadeOut: 750,
         responsive:true,
-        lensSize:50
+        lensSize:0
     });  
 
 });
